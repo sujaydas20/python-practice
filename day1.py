d2ff5462099743c755073f0f12692c5ecbea4058
sujaydas <sujaydas17595@gmail.com>
2026-09-24 21:28:08 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -15,3 +15,15 @@ x = d.pop(2)
 d[4] = x + d[3]
 
 print(sum(d.values()))
+
+
+
+s = 0
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        if i + j > 5:
+            continue
+        s += i * j
+
+print(s)
